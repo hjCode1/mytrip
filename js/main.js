e8f2b9a299f97f3f5719ccc39d6cd375e1083f08
hjCode1 <gogokye@naver.com>
2019-10-25 21:47:31 +0900

    템플릿 추가

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(function(){
     var _w = $(window);
     var _header = $('#header');
     var _from = $('#from');
+    var _to = $('#to');
 
     _w.scroll(function(){
         Header.move();
@@ -19,9 +20,65 @@ $(function(){
     _w.trigger('scroll');
 
     var dpFrom = _from.datepicker({
-        dateFormat: 'yy-mm-dd'
+        dateFormat: 'yy-mm-dd',
+        minDate: 0
     });
     dpFrom.datepicker('setDate', new Date());
+
+    var dpTo = _to.datepicker({
+        dateFormat: 'yy-mm-dd',
+        minDate: 0,
+        onSelect: function(){
+            dpTo.datepicker('option', 'minDate', dpFrom.datepicker('getDate'));
+        }
+    });
+    dpTo.datepicker('setDate', 4);
+
+    function search(from, to){
+        var url = 'https://javascript-basic.appspot.com/searchLocation';
+
+        $.getJSON(url,{
+            from: from,
+            to: to
+        }, function(r){
+            //console.log(r, from, to)
+            var $list = $('#list-panel');
+
+            for( var i = 0; i < r.length; i++ ){
+                var data = r[i];
+                var $item = createListItem(data);
+                //console.log( $item )
+                $list.append($item)
+            }
+
+            $('#list-bg').show();
+        });
+    }
+
+    $('#form-search').submit(function(e){
+        e.preventDefault(); // ajax로 페이지 이동없이 폼을 전송해야하므로 기본동작 방지
+
+        var from = _from.val();
+        var to = _to.val();
+
+        search(from, to);
+    });
+
+    function createListItem(data){
+        var $tmpl = $('#list-item-template').clone().removeAttr('id');
+
+        $tmpl.find('.list-item-image').attr('src', data.titleImageUrl);
+        $tmpl.find('.list-item-name').html(data.name);
+        $tmpl.find('.list-item-city-name').html(data.cityName);
+
+        $tmpl.click(function(e){
+            var url = 'detail.html?id=' + data.id;
+            window.location = url;
+        });
+
+        return $tmpl;
+    }
+
 
 
 });
